455a8b55744b235dec1b041be1599fd4c3b4067c
RMJacob173 <reeba.m.jacob@gmail.com>
2020-02-15 01:34:27 +0530

    Enable django admin for user profile model

--- profiles_app/models.py
+++ profiles_app/models.py
@@ -8,55 +8,54 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserProfileManager(BaseUserManager):
-    """Manager for user profile"""
-    #function to manipulate or make change to UserProfile
-    def create_user(self,email,name,password=None): #default password value is None
-        """Creating a new user"""
-        if not email:#empty or null
-            raise ValueError("User Must Have An Email Address")
+    """Manager for user profiles"""
 
-        #normalise email Address i.e makes the second half of the email address to lower
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('Users must have an email address')
+
         email = self.normalize_email(email)
-        user = self.model(email =email, name=name)
+        user = self.model(email=email, name=name,)
 
-        user.set_password(password) #encrots passord
-        user.save(using = self._db) #to consider support for multiple databases
+        user.set_password(password)
+        user.save(using=self._db)
+
         return user
 
-    #to create a super user
-    def create_superuser(self,email,name, password):
-        user = self.create_user(email,name,passord)
-        user.is_superuser = True #Automatically created by PermissionMixin
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
         user.is_staff = True
-        user.save(using = self._db)
+        user.save(using=self._db)
+
         return user
 
 
 
 
-class UserProfile(AbstractBaseUser,PermissionsMixin):
-    """Database models for the users in the system"""
-
-    email = models.EmailField(max_length=255, unique = True) #unique email id
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    #add files for the permission system
-    is_active = models.BooleanField(default = True)#allows to deactivate user
-    is_staff = models.BooleanField(default=False)#admin or users
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-    #to create a custom model manager
     objects = UserProfileManager()
 
-    #to override the default username field
-    USERNAME_FIELD ="email"
-    REQUIRED_FIELD =["name"]
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Return the fill name"""
+        """Retrieve full name for user"""
         return self.name
 
     def get_short_name(self):
-        """Return short name"""
+        """Retrieve short name of user"""
         return self.name
 
     def __str__(self):
-        return self.email # strng representing the users
+        """Return string representation of user"""
+        return self.email
